a3caf9b7034012b016031f8cf43ef5fb3c8dd8c4
carrillo21108 <87512111+carrillo21108@users.noreply.github.com>
2024-04-03 18:58:42 -0600

    Error léxico

--- ScanFrame.py
+++ ScanFrame.py
@@ -88,7 +88,7 @@ def tokensRecognize(afd,txtContent):
             resultado = resultado if resultado!=None else ""
             print(resultado)
         elif not res[0] and first!=len(txtContent):
-            message = f"-- ERROR -- al reconocer archivo txt en caracter no. {{res[1]+1}}: "
+            message = f"-- ERROR LEXICO -- al reconocer archivo txt en caracter no. {{res[1]+1}}: "
             posicion = ' '*len(message)
             for item in txtContent[first:res[1]]:
                 if item=='\\n':
